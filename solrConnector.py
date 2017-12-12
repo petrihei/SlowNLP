115bd3fc8c2a8de20bf8c563d443c0d0929c7dae
JohannaTK <nho6kx8d>
2017-12-12 19:23:49 +0200

    sovelluslogiikka

--- solrConnector.py
+++ solrConnector.py
@@ -8,6 +8,7 @@ def title_query(word):
 def get_results(word):
 
     # look for the closest matches
+
     query = word+'~'
     url = 'http://localhost:8983/solr/latin/select?q=title:('+query+')&rows=1&wt=python'
     connection = urlopen(url)
